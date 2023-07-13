4d76a43497d46ad9f91f299e2ec150bf3e4174cf
McCune1224 <alexmccune1224@gmail.com>
2023-07-13 16:50:57 -0400

    React Router boilerplate setup

--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -1,8 +1,23 @@
+import { useState } from 'react'
 
 const Root = () => {
-  return (
-    <div>root</div>
-  )
+    const [count, setCount] = useState(0)
+
+    const handleCounterPress = () => {
+        setCount((count) => count + 1)
+    }
+
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">Wow very cool</h1>
+            <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={handleCounterPress}
+            >
+                Count is: {count}
+            </button>
+        </div>
+    )
 }
 
 export default Root
