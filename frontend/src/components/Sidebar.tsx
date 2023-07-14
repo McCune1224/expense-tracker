43048fe0e432449f257ca025994d749fc838a8d0
McCune1224 <alexmccune1224@gmail.com>
2023-07-13 21:15:59 -0400

    flowbite is not the play. KISS babyyyy!

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,33 +1,3 @@
-import { Link } from 'react-router-dom'
-
-//TSX element for sidebar link component
-const SidebarLink = ({
-    to,
-    children,
-}: {
-    to: string
-    children: React.ReactNode
-}) => {
-    return (
-        <li className="mb-2 ">
-            <Link to={to}>{children}</Link>
-        </li>
-    )
+export default function DefaultSidebar() {
+    return <div>sidebar</div>
 }
-
-const Sidebar = () => {
-    return (
-        <div className="flex flex-col bg-gray-200 w-64">
-            <ul className="p-4">
-                <SidebarLink to="/dashboard/">Home</SidebarLink>
-                <SidebarLink to="/dashboard/transactions">
-                    Transactions
-                </SidebarLink>
-                <SidebarLink to="/dashboard/categories">Categories</SidebarLink>
-                <SidebarLink to="/dashboard/budgets">Budgets</SidebarLink>
-            </ul>
-        </div>
-    )
-}
-
-export default Sidebar
--- frontend/src/routes/dashboard.tsx
+++ frontend/src/routes/dashboard.tsx
@@ -1,9 +1,8 @@
-import { Outlet } from "react-router-dom"
-import Sidebar from "../components/Sidebar"
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../components/Sidebar'
 
 const Dashboard = () => {
     return (
-
         <>
             <Sidebar />
             <Outlet />
